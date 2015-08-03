84da078acee03e21762aa97a86a4108dc6323878
Sai Uday Shankar Korlimarla <skorlimarla@unomaha,edu>
2015-08-03 13:51:50 -0500

    More generic XML parsing
    More generic XML parsing

--- xml2mavenArtifact.py
+++ xml2mavenArtifact.py
@@ -1,39 +1,41 @@
-'''
-Author: Sai Uday Shankar
-'''
-
-# This is a very specific  XML Parser.
-# This parser requires any XML file to look like a maven pom.xml
-# For looking at a sample look at the below URL
-## https://maven.apache.org/guides/introduction/introduction-to-the-pom.html
-
 import xmltodict
 import re, sys
 
-def xmlmvnparser(xmlfile):
+def expand(xml, properties):
+    
+    for key in properties:   
+        if properties[key] is not None:
+            xml = xml.replace('${' + key + '}', properties[key])
+    return xml
+
+
+def xmlparser(xmlfile):
     xmldoc = open(xmlfile, 'r')
     xmlcont = xmldoc.read()
     xmldoc.close()
     tree = xmltodict.parse(xmlcont)
+    #print tree
+    #checktree(tree)
+    if(tree.get('project')):
+        if(tree['project'].get('properties')):
+            properties = tree['project']['properties']
+            xmlcont = expand(xmlcont, properties)
+            tree = xmltodict.parse(xmlcont)
+
+    if(tree.get('project')):    
+        dependencies = tree['project']['dependencies']
+    else:
+        dependencies = tree['dependencies']
     
-    g_id = tree['project']['groupId']
-    a_id = tree['project']['artifactId']
-    ver = tree['project']['version']
-    g = []
-    a = []
-    v = []
-    dependencies = tree['project']['dependencies']
     mav_coords = []
     for item in dependencies['dependency']:
+        
+        mav = str(item['groupId']) + ":" + str(item['artifactId']) + ":" + str(item['version'])
+	mav_coords.append(mav)
 
-        g.append(str(item['groupId']))
-        a.append(str(item['artifactId']))
-
-        v.append(str(item['version']))
-	
-    return(g_id, a_id, ver, g, a, v)
-
+    return(mav_coords)
 
 if __name__ =='__main__':
+
     xmlparser('pom.xml')
     
